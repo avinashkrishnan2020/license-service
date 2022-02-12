3e885a714e67ed6d884d09a1b5ec092f4f4b7df5
avinashkrishnan2020 <62984268+avinashkrishnan2020@users.noreply.github.com>
2022-02-13 02:12:17 +0530

    minor tweaks

--- src/main/java/com/eaglesoft/licensesrv/controller/LicenseController.java
+++ src/main/java/com/eaglesoft/licensesrv/controller/LicenseController.java
@@ -4,12 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/license-srv")
 public class LicenseController {
 
-    @GetMapping(value = "/getLicense")
+    @GetMapping(value = "/get-license")
     public String getLicense() {
         return "Hello";
     }
