f18fb1c056b1264421da8dce9344dfbe92c8d0d2
avinashkrishnan2020 <62984268+avinashkrishnan2020@users.noreply.github.com>
2022-02-13 00:04:58 +0530

    license-srv files

---
+++ src/main/java/com/eaglesoft/licensesrv/LicensesrvApplication.java
@@ -0,0 +1,13 @@
+package com.eaglesoft.licensesrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensesrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensesrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eaglesoft/licensesrv/LicensesrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaglesoft.licensesrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensesrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
